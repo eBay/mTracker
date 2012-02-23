858aaaf4354caf67dff1e5679991a41fc8d78438
Kamal <kmuralidharan@ebay.com>
2012-02-22 20:27:16 -0800

    Adding the Tracking Implementation project

---
+++ MavenTrackingListenerImpl/src/main/java/com/ebay/osgi/build/maven/TimeTrackingImpl.java
@@ -0,0 +1,117 @@
+package com.ebay.osgi.build.maven;
+
+import java.net.URL;
+
+import org.apache.maven.execution.AbstractExecutionListener;
+import org.apache.maven.execution.ExecutionEvent;
+
+import com.ebay.kernel.calwrapper.CalTransaction;
+import com.ebay.osgi.build.util.CALLoggerUtil;
+
+public class TimeTrackingImpl extends AbstractExecutionListener {
+	
+	private CalTransaction sessionTransaction;
+	private CalTransaction projectTransaction;
+	private CalTransaction mojoTransaction;
+	
+	
+	public TimeTrackingImpl() {
+		URL calConfig = getClass().getClassLoader().getResource("cal.properties");
+		CALLoggerUtil.initialize(calConfig);
+	}
+
+	@Override
+	public void projectDiscoveryStarted(ExecutionEvent event) {
+		
+	}
+
+	@Override
+	public void sessionStarted(ExecutionEvent event) {
+		sessionTransaction = CALLoggerUtil.startCALTransaction(event.getProject().getId(), event.getSession().getGoals().toString());
+	}
+
+	@Override
+	public void sessionEnded(ExecutionEvent event) {
+		CALLoggerUtil.endCALTransaction(sessionTransaction, "0");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		CALLoggerUtil.destroy();
+	}
+
+	@Override
+	public void projectSkipped(ExecutionEvent event) {
+		CALLoggerUtil.logCALEvent("SKIPPED", event.getProject().getId());
+	}
+
+	@Override
+	public void projectStarted(ExecutionEvent event) {
+		projectTransaction = CALLoggerUtil.startCALTransaction(event.getProject().getId(), event.getProject().getId());
+	}
+
+	@Override
+	public void projectSucceeded(ExecutionEvent event) {
+		CALLoggerUtil.endCALTransaction(projectTransaction, "0");
+	}
+
+	@Override
+	public void projectFailed(ExecutionEvent event) {
+		CALLoggerUtil.endCALTransaction(projectTransaction, "Failed", event.getException());
+	}
+	
+	@Override
+	public void mojoStarted(ExecutionEvent event) {
+		mojoTransaction = CALLoggerUtil.startCALTransaction(event.getMojoExecution().getPlugin().getId(), event.getMojoExecution().getPlugin().getId());
+	}
+
+	@Override
+	public void mojoSucceeded(ExecutionEvent event) {
+		CALLoggerUtil.endCALTransaction(mojoTransaction, "0");
+	}
+
+	@Override
+	public void mojoFailed(ExecutionEvent event) {
+		CALLoggerUtil.endCALTransaction(mojoTransaction, "Failed", event.getException());
+	}
+
+	@Override
+	public void forkStarted(ExecutionEvent event) {
+		
+	}
+
+	@Override
+	public void forkSucceeded(ExecutionEvent event) {
+		
+	}
+
+	@Override
+	public void forkFailed(ExecutionEvent event) {
+		
+	}
+
+	@Override
+	public void mojoSkipped(ExecutionEvent event) {
+		
+	}
+
+
+
+	@Override
+	public void forkedProjectStarted(ExecutionEvent event) {
+		
+	}
+
+	@Override
+	public void forkedProjectSucceeded(ExecutionEvent event) {
+		
+	}
+
+	@Override
+	public void forkedProjectFailed(ExecutionEvent event) {
+		
+	}
+	
+}
---
+++ MavenTrackingListenerImpl/src/main/java/com/ebay/osgi/build/util/CALLoggerUtil.java
@@ -0,0 +1,148 @@
+package com.ebay.osgi.build.util;
+
+import java.net.URL;
+
+import com.ebay.kernel.cal.CalServiceFactory;
+import com.ebay.kernel.cal.java.CalClientConfigBean;
+import com.ebay.kernel.cal.java.JavaCalService;
+import com.ebay.kernel.calwrapper.CalEvent;
+import com.ebay.kernel.calwrapper.CalEventFactory;
+import com.ebay.kernel.calwrapper.CalTransaction;
+import com.ebay.kernel.calwrapper.CalTransactionFactory;
+import com.ebay.kernel.calwrapper.CalTransactionHelper;
+
+/**
+ * CALLoggerUtil class provides methods for initialize CAL , methods for logging
+ * transaction and events.
+ * 
+ * @author kmuralidharan
+ *
+ */
+public final class CALLoggerUtil {
+	
+	private static final String CAL_TRAN_TYPE = "URL";
+	private static JavaCalService calService;
+	
+	private CALLoggerUtil() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param calConfig
+	 */
+	public static void initialize(URL calConfig) {
+		if (calConfig == null){
+			throw new IllegalArgumentException("calConfig URL cannot be Null");
+		}
+		CalClientConfigBean calClientCfgBean = new CalClientConfigBean(null,false,calConfig);
+		initialize(calClientCfgBean);
+	}
+	
+	/**
+	 * 
+	 * @param calClientCfgBean
+	 */
+	public static void initialize(CalClientConfigBean calClientCfgBean){
+		if(isCalInitialized()){
+			return;
+		}
+		if (calClientCfgBean == null){
+			throw new IllegalArgumentException("calConfig URL cannot be Null");
+		}
+		calService = new JavaCalService(calClientCfgBean, CalTransactionHelper.CAL_TRANSACTION_LEVEL);
+		CalServiceFactory.setCalService(calService);
+	}
+	
+	/**
+	 * destroy() method needs to be called at the end of the application.
+	 * This method is disconnect the CAL Service and terminate the process.
+	 */
+	public static void destroy()
+	{
+		if(calService != null){
+			try {
+				System.out.println(" Sleeping for 2 seconds .. ");
+				Thread.sleep(2*1000);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			} 
+			calService.disconnect();
+		}
+	}
+	
+	/**
+	 * Creates a CAL Transaction (default type URL) for the given transaction Name and data
+	 * 
+	 * @param transName name to identify the transaction
+	 * @param transData data which needs to be logged 
+	 * @return
+	 */
+	public static CalTransaction startCALTransaction(String transName, String transData) {
+		return createTransaction(transName, CAL_TRAN_TYPE, transData);
+	}
+	
+	/**
+	 * Creates a CAL Transaction for the given transaction Name, transaction Type and data
+	 * 
+	 * @param transName name to identify the transaction
+	 * @param transType type of the transaction
+	 * @param transData data which needs to be logged 
+	 * @return
+	 */
+	public static CalTransaction startCALTransaction(String transName,String transType, String transData) {
+		return createTransaction(transName, transType, transData);
+	}
+	
+	/**
+	 * Complete a CAL Transaction with a status
+	 * 
+	 * @param calTransaction
+	 * @param status
+	 */
+	public static void endCALTransaction(CalTransaction calTransaction, String status) {
+		calTransaction.setStatus(status);
+		calTransaction.completed();
+	}
+	
+	/**
+	 * Complete a CAL Transaction with a status
+	 * 
+	 * @param calTransaction
+	 * @param status
+	 */
+	public static void endCALTransaction(CalTransaction calTransaction, String status, Throwable t) {
+		calTransaction.setStatus(status);
+		calTransaction.setStatus(t);
+		calTransaction.completed();
+	}
+	
+	/**
+	 * log CAL Event with the given name and data
+	 * @param eventType Type of the event
+	 * @param data data to be logged
+	 */
+	public static void logCALEvent(String eventType, String data) {
+	    CalEvent event = CalEventFactory.create(eventType);
+	    event.setName(eventType);
+	    event.addData(data);
+	    event.setStatus("0");
+	    event.completed();
+	  }
+	
+	private static CalTransaction createTransaction(String transName, String transType, String transData) {
+		CalTransaction calTransaction = CalTransactionFactory.create(transType);
+		calTransaction.setName(transName);
+		calTransaction.addData(transData);
+		return calTransaction;
+	}
+	
+	/**
+	 * Answers if a CAL Service is initialized
+	 * 
+	 * @return
+	 */
+	public static boolean isCalInitialized(){
+		return calService != null;
+	}
+}
