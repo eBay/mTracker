e413387a1f650adb5c0657080c08e0428a15e064
Kamal <kmuralidharan@ebay.com>
2012-03-21 21:34:29 -0700

    Added Git URL, Git Branch for CI, Maven parameters Logged Discovery phase of maven

--- MavenTrackingListenerImpl/src/main/java/com/ebay/osgi/build/maven/TimeTrackingImpl.java
+++ MavenTrackingListenerImpl/src/main/java/com/ebay/osgi/build/maven/TimeTrackingImpl.java
@@ -1,5 +1,6 @@
 package com.ebay.osgi.build.maven;
 
+import java.io.File;
 import java.net.URL;
 import java.net.UnknownHostException;
 
@@ -8,9 +9,11 @@ import org.apache.maven.execution.ExecutionEvent;
 
 import com.ebay.kernel.calwrapper.CalTransaction;
 import com.ebay.osgi.build.util.CALLoggerUtil;
+import com.ebay.osgi.build.util.GitUtil;
 
 public class TimeTrackingImpl extends AbstractExecutionListener {
 	
+	private CalTransaction discoveryTransaction;
 	private CalTransaction sessionTransaction;
 	private CalTransaction projectTransaction;
 	private CalTransaction mojoTransaction;
@@ -24,34 +27,52 @@ public class TimeTrackingImpl extends AbstractExecutionListener {
 	@Override
 	public void projectDiscoveryStarted(ExecutionEvent event) {
 		
-	}
-
-	@Override
-	public void sessionStarted(ExecutionEvent event) {
 		StringBuilder data = new StringBuilder();
 		
-		data.append("Goals=").append(event.getSession().getGoals().toString());
-		
-		data.append(";username=").append(System.getProperty("user.name"));
+		data.append("goals=").append(event.getSession().getGoals().toString()).append(" ").append(event.getSession().getSystemProperties().toString());
 		
 		try {
 			data.append(";machine=").append(java.net.InetAddress.getLocalHost().getHostName());
 		} catch (UnknownHostException e) {}
+		
+		data.append(";username=").append(System.getProperty("user.name"));
+		
+		File gitMeta = GitUtil.findGitRepository(event.getSession().getTopLevelProject().getBasedir());
+		if(gitMeta != null && gitMeta.exists()) {
+			File gitConfig = new File(gitMeta, "config");
+			String gitURL =  GitUtil.getRepoName(gitConfig);
+			if(gitURL != null) {
+				data.append(";git_repo=").append(gitURL);
+			}
+		}
 
 		if(System.getenv("BUILD_URL") != null) {
-             data.append(";jenkinsurl=").append(System.getenv("BUILD_URL"));
+             data.append(";jenkins_url=").append(System.getenv("BUILD_URL"));
+             data.append(";git_branch").append(System.getenv("GIT_BRANCH"));
          }
+		discoveryTransaction = CALLoggerUtil.startCALTransaction("Project Discovery" , data.toString());
+	}
 
-		sessionTransaction = CALLoggerUtil.startCALTransaction("Session-" + event.getSession().getTopLevelProject().getId(), data.toString());
+
+	@Override
+	public void sessionStarted(ExecutionEvent event) {
+		sessionTransaction = CALLoggerUtil.startCALTransaction(event.getSession().getTopLevelProject().getId(), "SESSION", event.getSession().getStartTime().toString());
 	}
 
 	@Override
 	public void sessionEnded(ExecutionEvent event) {
-		CALLoggerUtil.endCALTransaction(sessionTransaction, "0");
+		
+		if(event.getSession().getResult().getExceptions().size() > 0) {
+			CALLoggerUtil.endCALTransaction(sessionTransaction, "FAILED", event.getSession().getResult().getExceptions().get(0));
+			CALLoggerUtil.endCALTransaction(discoveryTransaction, "FAILED", event.getException());
+		}
+		else {
+			CALLoggerUtil.endCALTransaction(sessionTransaction, "0");
+			CALLoggerUtil.endCALTransaction(discoveryTransaction, "0");
+		}
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		CALLoggerUtil.destroy();
@@ -64,7 +85,7 @@ public class TimeTrackingImpl extends AbstractExecutionListener {
 
 	@Override
 	public void projectStarted(ExecutionEvent event) {
-		projectTransaction = CALLoggerUtil.startCALTransaction("Project-" + event.getProject().getId(), "");
+		projectTransaction = CALLoggerUtil.startCALTransaction(event.getProject().getId(),"PROJECT", "");
 	}
 
 	@Override
@@ -79,7 +100,7 @@ public class TimeTrackingImpl extends AbstractExecutionListener {
 	
 	@Override
 	public void mojoStarted(ExecutionEvent event) {
-		mojoTransaction = CALLoggerUtil.startCALTransaction(event.getMojoExecution().getPlugin().getId(), "");
+		mojoTransaction = CALLoggerUtil.startCALTransaction(event.getMojoExecution().getPlugin().getId(), "PLUGIN", "");
 	}
 
 	@Override
---
+++ MavenTrackingListenerImpl/src/main/java/com/ebay/osgi/build/util/GitUtil.java
@@ -0,0 +1,65 @@
+package com.ebay.osgi.build.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class GitUtil {
+	
+	public static File findGitRepository( File currentDir ) {
+
+		File gitFolder=null;
+		File workDir = currentDir;
+		
+		while( workDir != null && workDir.exists() ){
+			if( isValidGitDir(workDir) ){
+				gitFolder = workDir;
+				break;
+			}else{
+				workDir = workDir.getParentFile();
+			}
+		}
+		
+		return gitFolder;
+	}
+	
+	
+	private static boolean isValidGitDir( File dir ){
+		boolean result = false;
+		
+		File gitDir = new File( dir, ".git");
+		
+		if( gitDir.exists() ){
+			if( gitDir.isDirectory() ){
+				if( gitDir.canRead() ){
+					result = true;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public static String getRepoName(File gitConfigFile) {
+		String repourl=null;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(gitConfigFile));
+			String line = "";
+			while((line = br.readLine()) != null) {
+				if(line.trim().startsWith("url")) {
+					repourl = line.split("=")[1].trim();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			
+		} catch (IOException e) {
+			
+		}
+		
+		return repourl;
+	}
+
+}
