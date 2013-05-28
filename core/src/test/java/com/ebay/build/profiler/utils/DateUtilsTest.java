1e2947ea1b4791e5544b17a1d798071ba6be365b
mmao <mmao@ebay.com>
2013-05-28 18:01:19 +0800

    add unit tests

--- core/src/test/java/com/ebay/build/profiler/utils/DateUtilsTest.java
+++ core/src/test/java/com/ebay/build/profiler/utils/DateUtilsTest.java
@@ -14,15 +14,15 @@ public class DateUtilsTest {
 		cal.set(Calendar.MONTH, Calendar.MAY);
 		cal.set(Calendar.DAY_OF_MONTH, 8);
 		
-		cal.set(Calendar.HOUR, 17);
+		cal.set(Calendar.HOUR_OF_DAY, 17);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
 		
-		assertEquals("09-05-2011 05:00:00", DateUtils.getCALDateTimeString(cal.getTime()));
+		assertEquals("08-05-2011 17:00:00", DateUtils.getCALDateTimeString(cal.getTime()));
 		
-		assertEquals("05:00:00.00", DateUtils.getCALTimeString(cal.getTime()));
+		assertEquals("17:00:00.00", DateUtils.getCALTimeString(cal.getTime()));
 		
-		assertEquals("09-05-2011 04:00:00", 
+		assertEquals("08-05-2011 16:00:00", 
 				DateUtils.getCALDateTimeString(DateUtils.getAnHourBack(cal.getTime()).getTime()));
 	}
 }
